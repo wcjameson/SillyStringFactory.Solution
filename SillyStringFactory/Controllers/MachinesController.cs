76b0359b11562ce9a45184e834aeb28d61681e46
wcjameson <williamjameson90@gmail.com>
2022-03-18 11:56:13 -0700

    add Index route for MachinesController

--- SillyStringFactory/Controllers/MachinesController.cs
+++ SillyStringFactory/Controllers/MachinesController.cs
@@ -3,3 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 using SillyStringFactory.Models;
 using System.Collections.Generic;
 using System.Linq;
+
+namespace SillyStringFactory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly SillyStringFactoryContext _db;
+
+    public MachinesController(SillyStringFactoryContext db)
+    {
+        _db = db;
+    }
+
+    public ActionResult Index()
+    {
+        List<Machine> model = _db.Machines.ToList();
+        return View(model);
+    }
+  }
+}
