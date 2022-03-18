a35a2b882632dc9d45c6572f00bdc683cc32f048
wcjameson <williamjameson90@gmail.com>
2022-03-18 11:47:52 -0700

    add AddMachine route for EngineersController

--- SillyStringFactory/Controllers/EngineersController.cs
+++ SillyStringFactory/Controllers/EngineersController.cs
@@ -77,5 +77,22 @@ namespace SillyStringFactory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+    public ActionResult AddMachine(int id)
+    {
+      var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Description");
+      return View(thisEngineer);
+    }
+
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int MachineId)
+    {
+      if (MachineId != 0)
+      {
+        _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
   }
 }
