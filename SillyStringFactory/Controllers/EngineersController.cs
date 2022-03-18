0bd0460ff9b0ffea9509d4d63430a0d4f194e35a
wcjameson <williamjameson90@gmail.com>
2022-03-18 10:29:07 -0700

    add index and create routes for EngineersController

---
+++ SillyStringFactory/Controllers/EngineersController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using SillyStringFactory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SillyStringFactory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly SillyStringFactoryContext _db;
+    public EngineersController(SillyStringFactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Description");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      if (MachineId != 0)
+      {
+        _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+  }
+}
--- SillyStringFactory/Models/Engineer.cs
+++ SillyStringFactory/Models/Engineer.cs
@@ -9,7 +9,6 @@ namespace SillyStringFactory.Models
     {
       this.JoinEntities = new HashSet<MachineEngineer>();
     }
-
     public int EngineerId { get; set; }
     public string Name { get; set; }
     public virtual ICollection<MachineEngineer> JoinEntities { get; }
