9ef14e978293a657856e3d27a299c7124a1d0da0
wcjameson <williamjameson90@gmail.com>
2022-03-18 12:01:35 -0700

    add Create route for MachinesController

--- SillyStringFactory/Controllers/MachinesController.cs
+++ SillyStringFactory/Controllers/MachinesController.cs
@@ -20,5 +20,17 @@ namespace SillyStringFactory.Controllers
         List<Machine> model = _db.Machines.ToList();
         return View(model);
     }
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+        _db.Machines.Add(machine);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
   }
 }
